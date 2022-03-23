a0439e071d1ed11ba44a29ffa5463e4b181c657a
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-03-22 20:20:49 -0500

    added all  unique categories as buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import './data';
-
+import data, { allCategories, categoriesUnique, namesAndCategories} from './data';
+import './components/CategoryButton'
+import CategoryButton from './components/CategoryButton';
 function App() {
 
-  
+  const buttons = categoriesUnique.map((cat) => {
+    return <CategoryButton name={cat} />
+  })
 
   return (
     <div className="App">
-      
+      {buttons}
     </div>
   );
 }
