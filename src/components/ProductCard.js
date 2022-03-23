a068efc847f8f507b47981b224935b19f98d7368
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-03-22 23:17:31 -0500

    completed all required challenges and most of stretch challenges

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,6 +3,8 @@ const ProductCard = (props) => {
     <div className="Product-card">
       <h4>{props.data.name}</h4>
       <p>{props.data.category}</p>
+      <p>{props.data.description}</p>
+      <p>Units: {(props.data.units)}</p>
       <p>{props.data.price}</p>
     </div>
   );
