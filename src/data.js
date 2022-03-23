5b69cc397806b54a5c557fc0ce6679ca2f1954bc
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-03-22 19:55:23 -0500

    completed 3 and 4

--- src/data.js
+++ src/data.js
@@ -1,6 +1,18 @@
 import data from './data.json'
 
+//grab the category from each object
 const allCategories = data.map(obj => obj.category)
-console.log(allCategories)
+
+//grab all unique categories, transform to an array
+const categorySet = new Set(allCategories)
+const categoriesUnique = Array.from(categorySet)
+
+const categoriesWithCounts = data.reduce((obj, category) => {
+    if (!obj.category) {
+        obj.category = 0
+    }
+    obj.category = obj.category++
+    return obj
+}, {})
 
 export default data
